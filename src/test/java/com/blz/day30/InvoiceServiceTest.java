9105a43ac0095a7b7e32a937d27da5bb3d196918
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-24 17:45:36 +0530

    Added : Program for the Calculate the fare for given distance and time

--- src/test/java/com/blz/day30/InvoiceServiceTest.java
+++ src/test/java/com/blz/day30/InvoiceServiceTest.java
@@ -4,5 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
+    }
 
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+
+    }
 }
