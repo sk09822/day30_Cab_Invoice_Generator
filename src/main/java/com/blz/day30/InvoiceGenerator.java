669b474c1d24f37609c5a788243503dd153b0703
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-24 17:42:21 +0530

    Cab invoice Service

---
+++ src/main/java/com/blz/day30/InvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.blz.day30;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Servic...!!!");
+    }
+    }
+
---
+++ src/test/java/com/blz/day30/InvoiceServiceTest.java
@@ -0,0 +1,8 @@
+package com.blz.day30;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+
+}
