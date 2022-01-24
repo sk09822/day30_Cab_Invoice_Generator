b553e9650c05e4f8e7d83adcac34d23e50539ee8
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-24 18:20:00 +0530

    Added : Program for the get the list from the ride repository

--- src/main/java/com/blz/day30/InvoiceGenerator.java
+++ src/main/java/com/blz/day30/InvoiceGenerator.java
@@ -5,30 +5,48 @@ public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1;
     private static final int MINIMUM_FARE = 5;
+    private RideRepository rideRepository;
 
     //calculate the total fare
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        if (totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        else
-            return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
+
+    public InvoiceGenerator() {
+        this.rideRepository = new RideRepository();
+    }
+
     //calculating Fare for Multiple rides
-    public double calculateFare(Ride[] rides){
-        double totalFare = 0;
-        for (Ride ride : rides){
-            totalFare += this.calculateFare(ride.distance, ride.time);
-        }
-        return totalFare;
-    }
-    //calculating totalFare, average Fare for multiple rides
-    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public InvoiceSummary calculateFare(Ride[] rides, String type) {
+        double totalFare = 0;
+        if (type == "premium") {
+            for (Ride ride : rides) {
+                totalFare += this.calculateFare(ride.distance, ride.time);
+            }
+            return new InvoiceSummary(rides.length, totalFare);
+        }
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
+
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRide(userId, rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId), "normal");
     }
 }
 
--- src/test/java/com/blz/day30/InvoiceServiceTest.java
+++ src/test/java/com/blz/day30/InvoiceServiceTest.java
@@ -12,42 +12,47 @@ public class InvoiceServiceTest {
         invoiceGenerator = new InvoiceGenerator();
     }
 
+    // testCase to write Total fare
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25, fare, 0.0);
     }
 
+    //testCase to calculate the Minimum Charges
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5, fare, 0.0);
-
     }
 
-    @Test
-    public void givenMultipleRides_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-        Ride[] rides = {new Ride(2.0, 5),
-                new Ride(0.1, 1)
-        };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(30, fare, 0.0);
-    }
-
+    //invoice generator for multiple rides
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-        Ride[] rides = {new Ride(5.0, 10),
-                new Ride(10, 20)
-        };
-        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
-        InvoiceSummary summary = new InvoiceSummary(2, 180);
-        Assertions.assertEquals(summary, invoiceSummary);
+        invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(25.0, 30), new Ride(12.0, 20)};
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 420);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
+    }
+
+    //testCase to return Invoice summary using userID
+    @Test
+    public void givenUserId_shouldReturnInvoiceSummary() {
+        invoiceGenerator = new InvoiceGenerator();
+        String userId = "sandipankande@gmail.com";
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        invoiceGenerator.addRides(userId, rides);
+        InvoiceSummary summary = invoiceGenerator.calculateFare(rides, "normal");
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(userId);
+        Assertions.assertEquals(invoiceSummary, summary);
+
     }
 }
