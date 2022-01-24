8f99262faede2086063d279a76075123ecc202cb
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-24 17:52:31 +0530

    Added : Program for the take multiple ride and calculate the aggragate the total fare

--- src/main/java/com/blz/day30/InvoiceGenerator.java
+++ src/main/java/com/blz/day30/InvoiceGenerator.java
@@ -6,12 +6,21 @@ public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
     private static final int MINIMUM_FARE = 5;
 
+    //calculate the total fare
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
         if (totalFare < MINIMUM_FARE)
             return MINIMUM_FARE;
         else
             return totalFare;
+    }
+    //calculating Fare for Multiple rides
+    public double calculateFare(Ride[] rides){
+        double totalFare = 0;
+        for (Ride ride : rides){
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
 
---
+++ src/main/java/com/blz/day30/Ride.java
@@ -0,0 +1,11 @@
+package com.blz.day30;
+
+public class Ride {
+    public final int time;
+    public double distance;
+    //parameterized Constructor
+    public Ride(double distance, int time){
+        this.distance = distance;
+        this.time = time;
+    }
+}
